f4e46b0abf7233b60fda08e26a1eda2621a7d760
Nurul Anam <51789412+nurulanam@users.noreply.github.com>
2023-02-08 17:58:46 +0600

    Recen Project On click Popup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,22 @@
 AOS.init();
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+$(document).ready(function() {
+    $('.img-pop').magnificPopup({
+        type:'image',
+        delegate: 'a',
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+            duration: 200, // duration of the effect, in milliseconds
+            // CSS transition easing function
+            opener: function(openerElement) {
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        },
+        
+    }
+
+    );
+  });
